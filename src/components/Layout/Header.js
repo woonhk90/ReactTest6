17191534f66006fd9369651946122845066a799b
woonhk90 <wonho7344@gmail.com>
2023-03-13 00:20:29 +0900

    '헤더' 컴포넌트 추가

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="mealsImage" />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;

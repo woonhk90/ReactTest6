f7e5e0b55ebfeadd904ddecddb0f6bec2cb8802b
woonhk90 <wonho7344@gmail.com>
2023-03-13 16:04:58 +0900

    '바구니' 버튼 컴포넌트 추가

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>??</span>
+    </button>
+  );
+};
+export default HeaderCartButton;

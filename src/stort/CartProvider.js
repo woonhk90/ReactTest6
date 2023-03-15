be1a8cec4a8f12c9b52489078bddfc18acfad290
woonhk90 <wonho7344@gmail.com>
2023-03-15 22:08:48 +0900

    장바구니 리듀서 추가

--- src/stort/CartProvider.js
+++ src/stort/CartProvider.js
@@ -1,11 +1,36 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "Add") {
+    const updateItems = state.items.concat(action.item);
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updateItems,
+      totalAmount: updateTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 /* ------------------ CartContext데이터를 관리, 접근하려는 모든 컴포넌트에 제공 ----------------- */
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
   const removeItemCartHandler = (id) => {};
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     remoteItem: removeItemCartHandler,
   };

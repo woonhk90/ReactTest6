f7ad72bb8d8020a456b0fed8a844c0b64dafca7c
woonhk90 <wonho7344@gmail.com>
2023-03-15 11:19:15 +0900

    Potal을 통해 모달 추가

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,25 @@
+import Modal from "../Ui/Modal";
+import modal from "../Ui/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = [{ id: "c1", name: "abc", amount: 2, price: 22.23 }];
   return (
-    <div>
+    <Modal>
       <ul className={classes["cart-items"]}>
         {cartItems.map((v, i) => (
-          <li>{v.name}</li>
+          <li key={v.id}>{v.name}</li>
         ))}
       </ul>
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>11.12</span>
       </div>
-      <div>
+      <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
